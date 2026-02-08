956fb0a249c9dffa98d3b0e2f9d2c483a0b99d01
devyinka <217584194+devyinka@users.noreply.github.com>
2026-02-08 04:01:20 +0100

    Update config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // 👈 REQUIRED for GitHub Pages
-
   images: {
-    unoptimized: true, // 👈 REQUIRED (next/image not supported on Pages)
     remotePatterns: [
       { protocol: "https", hostname: "res.cloudinary.com", pathname: "**" },
       { protocol: "https", hostname: "images.unsplash.com", pathname: "**" },
