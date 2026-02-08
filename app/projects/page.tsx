aae4b30cb94d2d0bc9793232dc137eeb30d623e4
devyinka <217584194+devyinka@users.noreply.github.com>
2026-02-08 05:13:31 +0100

    Further reduce spacing between projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,8 +4,8 @@ import { Project } from "../components/Project";
 
 export default function Projects() {
   return (
-    <section className="p-4 pb-5 max-w-6xl m-auto overflow-hidden 2xl:overflow-visible">
-      <div className="flex flex-col gap-4">
+    <section className="p-4 pb-2 max-w-6xl m-auto overflow-hidden 2xl:overflow-visible">
+      <div className="flex flex-col gap-2">
         {ProjectList.map((project: ProjectType) => (
           <Project key={`project-item-${project.title}`} {...project} />
         ))}
