12ec4e3078e3a2144c8dcccbac2d428c9ac51945
devyinka <217584194+devyinka@users.noreply.github.com>
2026-02-10 07:28:54 +0100

    modified:   app/utils/educationList.ts

--- app/utils/educationList.ts
+++ app/utils/educationList.ts
@@ -3,12 +3,12 @@ import { educationType } from "../types";
 export const educationList: educationType[] = [
   {
     career: "Self-Taught Full Stack Developer",
-    years: "2022 - Present",
+    years: "2023 - Present",
     description:
       "Built complex, deployable applications from scratch. Focused on modern web architecture, database management, and best practices in software design.",
   },
   {
-    career: "Telecommunication Engineering Student",
+    career: "Electronic and Telecommunication Engineering Student",
     years: "2021 - Present",
     description:
       "Federal University of Technology Minna, Niger State, Nigeria.",
